02b83a5bef8d86cd107ce8917c2a9018575e035f
Kristen Shea <kstar61@live.com>
2019-03-16 15:01:04 -0500

    /

--- checkifnumberisprime/Program.cs
+++ checkifnumberisprime/Program.cs
@@ -9,19 +9,17 @@ namespace checkifnumberisprime
             Console.WriteLine("Enter number:");
             int number = int.Parse(Console.ReadLine());
 
-            if (number < 2)
-            {
-                Console.WriteLine("True");
-                
-            }
+            if (number < 2) Console.WriteLine("False");
+
+            else if (number == 2) Console.WriteLine("True");
+
+            else if (number == 3) Console.WriteLine("True");
+
             else if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0)
-            {
                 Console.WriteLine("False");
-            }
-            else
-            {
-                Console.WriteLine("True");
-            }
+            
+            else Console.WriteLine("True");
+
 
             Console.ReadLine();
         }
