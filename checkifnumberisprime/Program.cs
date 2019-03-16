cc21b50c0b450e6e6c6c118a0fe9fcb416182794
Kristen Shea <kstar61@live.com>
2019-03-16 14:46:35 -0500

    /

--- checkifnumberisprime/Program.cs
+++ checkifnumberisprime/Program.cs
@@ -2,11 +2,28 @@
 
 namespace checkifnumberisprime
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter number:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 2)
+            {
+                Console.WriteLine("True");
+                
+            }
+            else if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+
+            Console.ReadLine();
         }
     }
 }
